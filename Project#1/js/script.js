2ce3a96ee6a9de4ae7c07adedb7bcb1fbc9b804d
Vitalii Vitamin <vitamin@ukr.net>
2018-02-16 16:49:27 +0200

    work

--- Project#1/js/script.js
+++ Project#1/js/script.js
@@ -1,9 +1,24 @@
 (function($) {
 $(function() {
+    
 
+	
+	
+	
+	
+	
+      $('ul.tabs-menu').on('click',  function(e) {
+
+      return false;
+
+  });
+
+    
+    
   $('ul.tabs-menu').on('click', 'li:not(.selected)', function(e) {
-    e.preventDefault();
-    $(this).addClass('selected').siblings().removeClass('selected');
+//    e.preventDefault();
+	$(this).parent().find('li').removeClass('selected');
+    $(this).addClass('selected');
     $tabs = $('.tabs-content');
       if($(this).hasClass('g')){
           $tabs.find('div.tabs-content__item').removeClass('selected');
@@ -59,9 +74,67 @@ $(function() {
     }
   ]
     });
+    
+    
+    
+   $tabs_slider = $('.tabs-menu');
+    
+    settings = {
+        slidesToShow: 1,
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-caret-left"></i></button>',
+      	nextArrow: '<button type="button" class="slick-next"><i class="fa fa-caret-right"></i></button>',
+    }
+    
+	$(window).on('resize load', function(){
+        if($(window).width() > 699){
+            if($tabs_slider.hasClass('slick-initialized')){
+                $tabs_slider.slick('unslick');
+            } return;
+        } else if(!$tabs_slider.hasClass('slick-initialize')){
+            return $tabs_slider.slick(settings);
+        }
+    });
+    
+	
+	$tabs_slider.on('afterChange', function(event, slick, currentSlide, nextSlide){
+		$current = $(this).find('.slick-current');
+		$current.addClass('selected').siblings().removeClass("selected");
+			$tabs = $('.tabs-content');
+			  if($current.hasClass('g')){
+				  $tabs.find('div.tabs-content__item').removeClass('selected');
+				  $tabs.find('div.g').addClass('selected');
+			  }
+			  else if($current.hasClass('w')){
+				  $tabs.find('div.tabs-content__item').removeClass('selected');
+				  $tabs.find('div.w').addClass('selected');
+			  }
+			  else if($current.hasClass('b')){
+				  $tabs.find('div.tabs-content__item').removeClass('selected');
+				  $tabs.find('div.b').addClass('selected');
+			  }
+			  else if($current.hasClass('v')){
+				  $tabs.find('div.tabs-content__item').removeClass('selected');
+				  $tabs.find('div.v').addClass('selected');
+			  }
+			  else if($current.hasClass('p')){
+				  $tabs.find('div.tabs-content__item').removeClass('selected');
+				  $tabs.find('div.p').addClass('selected');
+			  }
+			  else{
+				  $tabs.find('div.tabs-content__item').addClass('selected');
+			  }
+			  return false;
+	});
+	
+	
 	
     
-
+    
+    
+    $('.togglemenu').click(function(){
+        $(this).toggleClass('active'); 
+        $('.topmenu').slideToggle(400);
+    });
     
     
     
