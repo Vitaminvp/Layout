b4880438a52bf685254c641f996335816dee6ca5
Vitamin <Vitamin@ukr.net>
2018-02-18 23:01:15 +0200

    david

--- David landing/js/script.js
+++ David landing/js/script.js
@@ -9,14 +9,14 @@ $(function() {
 //      appendArrows: '.arrows',
       prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
       nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                dots: false
-              }
-            }
-        ]
+//        responsive: [
+//            {
+//              breakpoint: 1024,
+//              settings: {
+//                dots: false
+//              }
+//            }
+//        ]
     });
 });
 
