1ebc3612e40355a1b2f11a08a4d55f6df02fb554
Vitamin <Vitamin@ukr.net>
2018-02-21 07:19:03 +0200

    14

--- tack/js/script.js
+++ tack/js/script.js
@@ -108,7 +108,7 @@ function setNullsBefore(m, n) {
 
 function timerStart() {
 
-	startTime = 12000;       // Время задаеться здесь.
+	startTime = 120000;       // Время задаеться здесь.
 
 	timerTime = setInterval(
 		function () {
