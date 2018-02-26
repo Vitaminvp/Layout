6fd6c8c0c0a4baa24648ac90ce0f26e82b15e038
Vitalii Vitamin <vitamin@ukr.net>
2018-02-26 18:15:55 +0200

    26

--- miami/js/script.js
+++ miami/js/script.js
@@ -5,6 +5,72 @@ $(document).ready(function(){
 //        var moveY=(e.pageY * -1/20);
 //        $(this).css("background-position", moveX + "px " +moveY + "px");
 //      });
+	
+	$('a[href^="#"]').click(function(){
+		let target = $(this).attr("href");
+		$('html, body').animate({
+			scrollTop: $(target).offset().top - 20
+		}, 800);
+	});
+	
+	$(window).scroll(function(){
+		let top = $('.header-bottom').offset().top;
+		$(window).scroll(function(){
+			if( $(window).scrollTop() > top) {
+				$('.header-bottom').addClass('toTop');
+			}else{
+				$('.header-bottom').removeClass('toTop');
+			}
+		});
+	});
+	
+	$('#inlinebtn').click(function(e){
+//		e.preventDefault();
+		$(this).remove();
+		return false;
+	});
+	
+	
+	
+	
+	
+	$('.t-items').slick({
+	  infinite: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  dots: true,
+		responsive: [
+		{
+		  breakpoint: 1024,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			infinite: true,
+	  		dots: false
+		  }
+		},
+		{
+		  breakpoint: 720,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 1,
+	  		dots: false
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			dots: false
+		  }
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	  ]
+	});
+	
 });
 
     
