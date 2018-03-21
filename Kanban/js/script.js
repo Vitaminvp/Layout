e0d729856c370d35873e268006922efb2509fdd5
Vitalii Vitamin <vitamin@ukr.net>
2018-03-21 17:17:48 +0200

    20

--- Kanban/js/script.js
+++ Kanban/js/script.js
@@ -4,32 +4,32 @@ $(document).ready(function () {
 
     // Render bottom border for input field
     function colorBorder() {
-        $("input[type=text]").css("border-bottom", "2px solid" + color + "");
+        $("input[type=text]").css("border", "2px solid" + color + "");
     }
 
-function formatDate() {
-  let date = new Date(); 
-  let days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
-  let sec = Math.floor(date / 1000); // округлить  до секунд
-  let min = Math.floor(date / 60000); // округлить  до минут
+ 	// форматировать дату, с учетом того, что месяцы начинаются с 0
+	function formatDate() {
+		let date = new Date();
+		let days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+		let sec = Math.floor(date / 1000); // округлить  до секунд
+		let min = Math.floor(date / 60000); // округлить  до минут
 
 
-  // форматировать дату, с учетом того, что месяцы начинаются с 0
-  var d = date;
-  d = [
-    '0' + d.getDate(),
-    '0' + (d.getMonth() + 1),
-    '' + d.getFullYear(),
-    '0' + d.getHours(),
-    '0' + d.getMinutes()
-  ];
+		var d = date;
+		d = [
+		'0' + d.getDate(),
+		'0' + (d.getMonth() + 1),
+		'' + d.getFullYear(),
+		'0' + d.getHours(),
+		'0' + d.getMinutes()
+	  ];
 
-  for (var i = 0; i < d.length; i++) {
-    d[i] = d[i].slice(-2);
-  }
+		for (var i = 0; i < d.length; i++) {
+			d[i] = d[i].slice(-2);
+		}
 
-  return d.slice(0, 3).join('.') + ' ' + days[date.getDay()] + ' ' + d.slice(3).join(':');
-}
+		return d.slice(0, 3).join('.') + ' ' + days[date.getDay()] + ' ' + d.slice(3).join(':');
+	}
 
     // Define card
     function card(color, text) {
@@ -57,9 +57,22 @@ function formatDate() {
     $("input[type=text]").keypress(function (event) {
         if (event.which === 13) {
             let text = $(this).val();
-            $(this).val("");
-            $(this)
-                .next(".list-body")
+			if ( text !== "" ){
+				$(this).val("");
+				$(this)
+					.parent().next(".list-body")
+					.append(card(color, text));
+			}
+        }
+    });
+	
+    $("input[type=text]+button").click(function (event) {
+		let input = $('input[type=text]');
+        if (input.val() !== "") {
+            let text = input.val();
+            input.val("");
+            input
+                .parent().next(".list-body")
                 .append(card(color, text));
         }
     });
@@ -78,7 +91,7 @@ function formatDate() {
         $(this)
             .children()
             .toggleClass("chevron-top chevron-bottom");
-        $("input[type=text]").slideToggle();
+        $("input[type=text]").parent().slideToggle();
     });
 
     // Toggle chosen color
@@ -92,10 +105,34 @@ function formatDate() {
     });
 
     $(function () {
-        $("#sortable1, #sortable2, #sortable3")
+        $("#sortable1")
             .sortable({
-                connectWith: ".list-body"
+				connectWith: ".list-body, .list-body2",
             })
             .disableSelection();
     });
+	
+    $(function () {
+        $("#sortable2")
+            .sortable({
+				connectWith: ".list-body2, .list-body3",
+            })
+            .disableSelection();
+    });
+	
+//	$(function () {
+//        $("#sortable3")
+//            .sortable({
+//				connectWith: ".list-body2, .list-body3",
+//            })
+//            .disableSelection();
+//    });
+//	
+//    $(function () {
+//        $("#sortable1, #sortable2, #sortable3")
+//            .sortable({
+//                connectWith: ".list-body"
+//            })
+//            .disableSelection();
+//    });
 });
