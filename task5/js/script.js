cb889b85a7673273eef6fdb5bde73ff72ddc2cf0
Vitalii Vitamin <vitamin@ukr.net>
2018-03-29 17:55:21 +0300

    29

--- task5/js/script.js
+++ task5/js/script.js
@@ -67,13 +67,12 @@ console.log(el);
 console.log(el.length);
 
 for(var i=0; i<el.length; i++) {
-	console.log(el[i]);
    el[i].addEventListener("click", showSub, false);
 	
 }
 function showSub(e) {
-
-      this.classList.add('active');
+	  
+      e.target.classList.add('active');
 
 }
 
